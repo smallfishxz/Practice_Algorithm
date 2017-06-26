4e0625d7ca4a6e1a21050943114375f9ac584c4d
smallfishxz <xiaozheyu@gmail.com>
2017-06-25 20:42:03 -0700

    Update ***Stock_n_trades_cost.py

--- ***Stock_n_trades_cost.py
+++ ***Stock_n_trades_cost.py
@@ -1,20 +1,20 @@
-int findProfit(int[] input, int cost) {
-  if (input.length == 0) { 
-    return 0; 
-  }
-  var profit = 0;
-  var min = input[0];
-  var max = input[0];
-  for (var i = 1; i < input.length; i++) {
-    if (max - input[i] >= cost) {
-      profit += Math.max(0, max - min - cost);
-      max = min = input[i];
-    }
-    max = Math.max(max, input[i]);
-    if (input[i] < min) {
-      max = min = input[i];
-    }
-  }
-  profit += Math.max(0, max - min - cost);
-  return profit;
-}
+# int findProfit(int[] input, int cost) {
+#   if (input.length == 0) { 
+#     return 0; 
+#   }
+#   var profit = 0;
+#   var min = input[0];
+#   var max = input[0];
+#   for (var i = 1; i < input.length; i++) {
+#     if (max - input[i] >= cost) {
+#       profit += Math.max(0, max - min - cost);
+#       max = min = input[i];
+#     }
+#     max = Math.max(max, input[i]);
+#     if (input[i] < min) {
+#       max = min = input[i];
+#     }
+#   }
+#   profit += Math.max(0, max - min - cost);
+#   return profit;
+# }
