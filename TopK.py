5057eee067b2a4a17b5bf5d29025aef1581cf3f5
smallfishxz <xiaozheyu@gmail.com>
2017-07-08 17:42:06 -0700

    Create TopK.py

---
+++ TopK.py
@@ -0,0 +1,12 @@
+import heapq
+
+def topK(A,k):
+  h=[]
+  for i in range(0,k-1):
+    heapq.heappush(h,A[i]) 
+  for i in range(k, len(A)-1):
+    if A[i] > h[0]:
+      heapq.heappushpop(h,A[i])
+  return h
+  
+print(topK([10,7,6,5],3))
