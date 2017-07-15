fef2dcc883ff309295a8a742f047a4b5929b35a3
smallfishxz <xiaozheyu@gmail.com>
2017-07-14 22:15:53 -0700

    Create BSearch_SortedA.py

---
+++ BSearch_SortedA.py
@@ -0,0 +1,14 @@
+def b_search(A, n):
+    l=0
+    r=len(A)-1
+    while l<=r:
+        mid = (l+r)/2
+        if A[mid]==n:
+            return mid
+        elif A[mid]<n:
+            l=mid+1
+        else:
+            r=mid-1
+    return -1
+
+print b_search([2,3,4,10,40], 10)
