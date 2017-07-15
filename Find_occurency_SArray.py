e8060f93b34b2d8c31786e742488b6dbddbb9bba
smallfishxz <xiaozheyu@gmail.com>
2017-07-15 02:11:14 -0700

    Update Find_occurency_SArray.py

--- Find_occurency_SArray.py
+++ Find_occurency_SArray.py
@@ -1,3 +1,21 @@
+# Count of number K in Sorted Int ArrayeditEdittrashDelete
+# Have a sorted int array which might have repeated numbers, given number K, return count of K in the array. e.g. 12377799, K = 3 => 1, K = 7 => 3, K = 4 => 0.
+
+# Difficulty:  Medium
+# Owner:  Yi Sun
+# Solution 1 #
+# Hashmap, or linear scan O(n)
+
+# Solution 2 #
+# Binary Search + Linear Expand Avg O(lgn), worst O(n)
+
+# Solution 3 #
+# 2 Binary Search to find 1st, last element O(lgn)
+
+# ...and more
+
+# Usually finish solution 3 and consider all corner cases would be good hire signal.
+
 def find_first(A,n):
   l=0
   h=len(A)-1
