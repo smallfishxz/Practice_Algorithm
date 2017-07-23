fd8118b15de307111a887d187be455bcc6f2036f
smallfishxz <xiaozheyu@gmail.com>
2017-07-22 21:28:37 -0700

    Update Count_Interpretation_Cache.py

--- Count_Interpretation_Cache.py
+++ Count_Interpretation_Cache.py
@@ -1,28 +1,33 @@
 # Cache reference: https://codereview.stackexchange.com/a/95556
 
-cache={}
-pos=0
-def find_count(A, pos):
-    ll = len(A)-pos
+# Improving time complexity
+# The first simple improvement is to note that there are a lot of repeated computations and to store the result of a subproblem in a cache 
+# (aka. "memoization") and to look that up before calling the function recursively.
+# This reduces the time complexity from exponential to linear since we need to compute the result for a specific subproblem only once.
+# The space complexity is still linear, but we have used O(n) extra space.
+
+def find_count(A, pos, cache):
+    if pos in cache:
+      return cache[pos]
     
-    if ll==0:
+    ll = len(A)
+    
+    if pos==ll:
         return 1
     
-    if A[0]=='0':
+    if A[pos]=='0':
         return 0
     
-    if ll==1:
+    if pos==ll-1:
         return 1
     
-    count=find_count(A[pos+1:ll], pos)
+    count=find_count(A, pos+1, cache)
     
     if int(A[pos:pos+2])<=26:
-        count+=find_count(A[pos+2:ll], pos)
+        count+=find_count(A, pos+2, cache)
+    
+    cache[pos] = count
+    print(cache)
     return count
 
-def find_count_m(A):
-    if pos not in cache:
-        cache[pos]=find_count(A, pos)
-    return cache[pos]
-
-print find_count_m('21')
+print(find_count('111', 0, {}))
