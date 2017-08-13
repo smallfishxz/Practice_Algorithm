7e4d4954c81a74779b9626378ce67066739f5635
smallfishxz <xiaozheyu@gmail.com>
2017-08-12 22:19:11 -0700

    Update Bubble_sort.py

--- Sort/Bubble_sort.py
+++ Sort/Bubble_sort.py
@@ -1,3 +1,5 @@
+# references: http://www.geeksforgeeks.org/bubble-sort/
+
 def bubble_sort(A):
   for i in range(0,len(A)-1):
     swap_flag = False
@@ -5,9 +7,11 @@ def bubble_sort(A):
       if A[j]>A[j+1]:
         A[j], A[j+1] = A[j+1], A[j]
         swap_flag = True
-      print('(Outer loop {l1}, inner lopp {l2})'.format(l1=i, l2=j))
-      print(A)
-      if swap_flag == False:
-        break
+      # print('(Outer loop {l1}, inner lopp {l2})'.format(l1=i, l2=j))
+      # print(A)
+    # if no swap happens for loop j, A is already sorted, break outer loop
+    if swap_flag == False:
+      break
+  return A
 
-bubble_sort([10,9,7,6,5])
+print(bubble_sort([10,9,7,6,5]))
