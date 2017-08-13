ab09baaa20356b243d5b278a0348696db1f695c6
smallfishxz <xiaozheyu@gmail.com>
2017-08-12 22:57:59 -0700

    Update TopK.py

--- TopK.py
+++ TopK.py
@@ -3,8 +3,10 @@ import heapq
 # O: (n-k)logk, space: K
 def topK(A,k):
   h=[]
+  # Use Python native heap to build a min heap for the first k elements of the array
   for i in range(0,k-1):
     heapq.heappush(h,A[i]) 
+  # Loop through the element after kth element. If it is bigger than the min in heap, pop heap and push that element into heap  
   for i in range(k, len(A)-1):
     if A[i] > h[0]:
       heapq.heappushpop(h,A[i])
@@ -13,7 +15,8 @@ def topK(A,k):
 # O: nk, space: 0
 def topk_bubble_sort(A,k):
   ll = len(A)
-  for i in range(ll):
+  # Modify the outer loop of bubble sort for k times instead of len(A)
+  for i in range(k):
     swap_flag = False
     for j in range(0, ll-i-1):
       if A[j] > A[j+1]:
@@ -22,6 +25,8 @@ def topk_bubble_sort(A,k):
     if swap_flag == False:
       break
   return A[ll-k:]
+
+# print(topk_bubble_sort([10,7,6,5],4))
 
 print(topK([10,7,6,5],3))
 
