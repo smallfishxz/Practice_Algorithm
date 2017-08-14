048d049332e3682eb57c4a6e16d0d4efde7f1e75
smallfishxz <xiaozheyu@gmail.com>
2017-08-14 00:44:00 -0700

    Update Tree_height_rec_iter.py

--- Tree/Tree_height_rec_iter.py
+++ Tree/Tree_height_rec_iter.py
@@ -52,6 +52,29 @@ def height1(tree):
         que.append(node.right)
       node_count -= 1
     print('-'.join(map(node_value,que)))
+
+# more intuitive modification on top of the iterative tree height function
+def height2(tree):
+  if tree == None:
+    return 0
+  # initialize node queue as empty and height as 0
+  que = []
+  h = 0
+  # start from tree root to push it to node queue
+  que.append(tree)
+  
+  # Just a very small change to the iterative tree height function, that is, to have a variable to record the count of nodes in the current level, so that we could maintain a queue to always include nodes at the same level
+  while len(que) > 0:
+    print('-'.join(map(node_value, que)))
+    node_count = len(que) # minor change to height function
+    for i in range(node_count): # minor change to height function
+      node = que.pop(0)
+      if node.left is not None:
+        que.append(node.left)
+      if node.right is not None:
+        que.append(node.right)
+    h += 1
+  return h
     
 sub_tree_l = tree('B', tree('D'))
 sub_tree_r = tree('C', tree('E'), tree('F'))
