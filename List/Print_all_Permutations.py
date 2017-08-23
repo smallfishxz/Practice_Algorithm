a1a8f230d51f57bfbd3ca9eb292c02506835206b
smallfishxz <xiaozheyu@gmail.com>
2017-08-22 18:30:27 -0700

    Create Print_all_Permutations.py

---
+++ List/Print_all_Permutations.py
@@ -0,0 +1,3 @@
+# https://our.intern.facebook.com/intern/recruiting/interview/questions/?question_id=497025640507325
+
+# http://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
