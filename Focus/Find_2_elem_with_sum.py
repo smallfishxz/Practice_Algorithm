fd43c82c69aa5148f45ab978f209f38676ce2390
smallfishxz <xiaozheyu@gmail.com>
2017-09-06 23:55:50 -0700

    Create Find_2_elem_with_sum.py

---
+++ Focus/Find_2_elem_with_sum.py
@@ -0,0 +1,50 @@
+# Is sorted array? dup elements allowed? 
+
+# O(n*n)
+def solution(input, sum):
+    for i in range(0, len(input)):
+        for j in range(1, len(input)):
+            if (input[i] + input[j] == sum):
+                return True
+    return False
+
+# O(N * log N)
+def solution2(input, sum):
+    sorted(input)
+    i = 0
+    j = len(input) - 1
+    
+    while (i < j):
+        total = input[i] + input[j]
+        if (total == sum):
+            return True
+        elif (total > sum):
+            j=j-1
+        elif (total < sum):
+            i=i+1
+    return False
+
+# O(N). Build and search the dictionary while looping through the array
+def find_elems_with_sum(A, sum):
+  dic = {}
+  for elem in A:
+    # if (sum - elem) in dic.keys():
+    if dic.get(sum-elem):
+      return True
+    dic[elem] = True
+    print(dic)
+  
+  return False
+
+# O(N). dictionary is not quite necessay, could replace dictionary with a set
+def find_elems_with_sum1(A, sum):
+  s = set()
+  for item in A:
+    if (sum - item) in s:
+      return True
+    s.add(item)
+  return False
+      
+
+print(find_elems_with_sum1([1,2,4], 6))
+    
